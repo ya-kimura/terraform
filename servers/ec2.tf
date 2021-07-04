cec8d1abce12a6d1cbacb9878169df98342881c4
ya <yassui.kimura@gmail.com>
2021-07-03 23:29:56 -0300

    feat: add bloco dinamico

--- servers/ec2.tf
+++ servers/ec2.tf
@@ -19,7 +19,16 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
+  dynamic "ebs_block_device" {
+    for_each = var.blocks
+    content {
+      device_name = ebs_block_device.value["device_name"]
+      volume_size = ebs_block_device.value["volume_size"]
+      volume_type = ebs_block_device.value["volume_type"]
+    }
+  }
+
   tags = {
-    Name = "terraform"
+    Name = "ec2-terraform"
   }
 }
